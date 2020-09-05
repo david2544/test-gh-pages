3bdb4a9d83bd3774896a19c5d9153774c86d7251
david.c <david.coroian@raisin.com>
2020-09-05 19:35:49 +0200

    Updates

---
+++ precache-manifest.b0e1294a515f822ccdfd28839c8a7e07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c78b304cc6e11ab88ec48c160e7ff17",
+    "url": "/test-gh-pages/index.html"
+  },
+  {
+    "revision": "9acd476a4ce79416bbb4",
+    "url": "/test-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44048ae0dd0942cfd878",
+    "url": "/test-gh-pages/static/js/2.9e41738e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-gh-pages/static/js/2.9e41738e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9acd476a4ce79416bbb4",
+    "url": "/test-gh-pages/static/js/main.5ec97f51.chunk.js"
+  },
+  {
+    "revision": "9cdfa4b1e366448b1065",
+    "url": "/test-gh-pages/static/js/runtime-main.4df5fc5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
